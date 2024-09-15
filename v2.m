9cbd87c1579cd98fb1dce5f376fe7841e4276fc1
weilinyin <weilinyin@mail.nwpu.edu.cn>
2024-09-15 20:33:06 +0800

    Update v2.m

--- v2.m
+++ v2.m
@@ -1,6 +1,6 @@
 % 定义传递函数
 s = tf('s');
-G = 1 / s^2;  % 液压伺服机构的传递函数
+G = 1 / s;  % 液压伺服机构的传递函数
 G1= (2 * s + 0.1) / (s^2 + 0.1 * s +4); % 飞机的传递函数
 H = 1;  % 速率脱落传递函数
 
@@ -23,10 +23,15 @@ plot(real(p1), imag(p1), 'rx', 'MarkerSize', 10, 'LineWidth', 2);
 plot(real(p2), imag(p2), 'rx', 'MarkerSize', 10, 'LineWidth', 2);
 hold off;
 
+%计算相角差额
+[~, ~, poles] = damp(OLTF);
+phi=3*pi-angle(p1-poles(1))-angle(p1-poles(2))-angle(p1-poles(3))-angle(p1+0.05);
+alpha=phi*0.99;
+
 % 设计超前校正器 G_c(s)
-z = 0;  % 补偿器零点
-p = ;    % 补偿器极点
-K = ;    % 初始增益
+z = 2*sqrt(3)/tan(alpha)+2;  % 补偿器零点
+p = 2*sqrt(3)/tan(phi-alpha)+2;    % 补偿器极点
+K = abs((p1+p)*(p1^2+0.1*p1+4)*p1/((2*p1+0.1)*(p1+z)));    % 初始增益
 G_c = K * (s + z) / (s + p);  % 超前校正器
 
 % 更新后的开环传递函数
@@ -44,7 +49,7 @@ hold off;
 
 % 调整增益，检查闭环极点的位置
 
-CLTF = feedback(OLTF_compensated, 1);
+CLTF = feedback(G_c * G * G1, H);
 
 % 验证设计：绘制阶跃响应
 figure;
@@ -65,3 +70,4 @@ fprintf('稳定时间: %.2f 秒\n', settling_time);
 [wn, zeta, poles] = damp(CLTF);
 fprintf('自然频率 (rad/s): %.2f\n', wn(2));
 fprintf('阻尼比: %.2f\n', zeta(2));
+poles
